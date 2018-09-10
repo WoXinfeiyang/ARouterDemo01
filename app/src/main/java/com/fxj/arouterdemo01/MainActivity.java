4fe73db32fd5455e78a2bab6982aea9d41231c8b
fuxianjin <fuxianjin-hj@huajiao.tv>
2018-09-10 18:43:57 +0800

    增加跳转回调

--- app/src/main/java/com/fxj/arouterdemo01/MainActivity.java
+++ app/src/main/java/com/fxj/arouterdemo01/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.alibaba.android.arouter.facade.Postcard;
+import com.alibaba.android.arouter.facade.callback.NavCallback;
 import com.alibaba.android.arouter.launcher.ARouter;
 
 public class MainActivity extends Activity{
@@ -22,7 +24,33 @@ public class MainActivity extends Activity{
         switch (v.getId()){
             case R.id.navigateToAppModule:
                 Log.i(tag,"R.id.navigateToAppModule被点击了!");
-                ARouter.getInstance().build("/app/AppActivity1").navigation();
+                ARouter.getInstance().build("/app/AppActivity1").navigation(this, new NavCallback() {
+                    @Override
+                    public void onFound(Postcard postcard) {
+                        // Do nothing
+                        Log.i(tag,"**onFound**"+postcard.toString());
+                    }
+
+                    @Override
+                    public void onLost(Postcard postcard) {
+                        // Do nothing
+                        Log.i(tag,"**onFound**"+postcard.toString());
+                    }
+
+
+                    @Override
+                    public void onArrival(Postcard postcard) {
+                        Log.i(tag,"**onFound**"+postcard.toString());
+                    }
+
+                    @Override
+                    public void onInterrupt(Postcard postcard) {
+                        // Do nothing
+                        Log.i(tag,"**onFound**"+postcard.toString());
+                    }
+
+
+                });
                 break;
             case R.id.navigateToOtherModule:
                 Log.i(tag,"R.id.navigateToOtherModule被点击了!");
